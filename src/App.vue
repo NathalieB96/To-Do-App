fce3519dee49946ed9d46b32ae71b1e4e0a93814
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-03 20:21:40 +0100

    Mark todo as done
    - changed card look: green background, line through
    - added toggle: done/not done

--- src/App.vue
+++ src/App.vue
@@ -39,17 +39,25 @@
   <div
     v-for="todo in todos"
     class="card mb-5"
+    :class="{ 'has-background-success-light' : todo.done}"
     >
     <div class="card-content">
       <div class="content">
 
         <div class="columns is-mobile is-vcentered">
-          <div class="column">
+          <div 
+            class="column"
+            :class="{'has-text-success line-through' : todo.done}"
+          >
             {{todo.content}}
 
           </div>
           <div class="column is-5 has-text-right">
-            <button class="button is-light">
+            <button 
+              @click="toggleDone(todo.id)"
+              class="button"
+              :class="todo.done ? 'is-success' : 'is-light'"
+            >
               &check; 
             </button>
             <button 
@@ -83,16 +91,16 @@ import { v4 as uuidv4 } from 'uuid';
 */
 
 const todos = ref([
-  {
-    id: 'id1',
-    content: 'buy apples',
-    done: false
-  },
-  {
-    id: 'id2',
-    content: 'eat my apples',
-    done: false
-  }
+  // {
+  //   id: 'id1',
+  //   content: 'buy apples',
+  //   done: false
+  // },
+  // {
+  //   id: 'id2',
+  //   content: 'eat my apples',
+  //   done: true
+  // }
 ])
 
 /*
@@ -120,6 +128,17 @@ const deleteTodo = id => {
   todos.value = todos.value.filter(todo => todo.id !== id)
 }
 
+
+/*
+  toggle done
+*/
+
+const toggleDone = id => {
+  const index = todos.value.findIndex(todo => todo.id === id)
+  todos.value[index].done = !todos.value[index].done
+}
+
+
 </script>
 
 
@@ -130,6 +149,10 @@ const deleteTodo = id => {
     max-width: 400px;
     padding: 20px;
     margin: 0 auto;
+  }
+
+  .line-through {
+    text-decoration: line-through;
   }
 </style>
 
