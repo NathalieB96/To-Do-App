03c46c7d36eb4d48d25928fa2b83fb355b0f53b6
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-03 20:26:15 +0100

    Install Firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKTwdEQYkeSzNsSGN-EMoZG9ghiGS1zFA",
+  authDomain: "to-do-app-53bd6.firebaseapp.com",
+  projectId: "to-do-app-53bd6",
+  storageBucket: "to-do-app-53bd6.appspot.com",
+  messagingSenderId: "172241751342",
+  appId: "1:172241751342:web:4413cdbbf2c473d74b3ebf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
