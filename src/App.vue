0430c502d2e3e09346dc9f79d500c6ac248b7bc4
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-05 12:54:04 +0100

    Revert "Mark todo as Done"
    This reverts commit 2825f04f9be8e0ece46fbc861f1f023c3505c5d6.

--- src/App.vue
+++ src/App.vue
@@ -84,7 +84,7 @@
 */
 
 import { ref, onMounted } from 'vue';
-import { collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { collection, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from '@/firebase';
 
 /*
@@ -164,10 +164,7 @@ const deleteTodo = id => {
 
 const toggleDone = id => {
   const index = todos.value.findIndex(todo => todo.id === id)
-
-    updateDoc(doc(todosCollectionRef, id), {
-      done: !todos.value[index].done
-    });
+  todos.value[index].done = !todos.value[index].done
 }
 
 
