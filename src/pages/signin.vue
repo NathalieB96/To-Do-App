479b624a26c3853f209527710a4aff652b9630b6
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-06 13:27:40 +0100

    Sign In Setup

---
+++ src/pages/signin.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Sign in to your Account</h1>
+    <p><input type="text" placeholder="E-mail" v-model="email"/></p>
+    <p><input type="password" placeholder="Password" v-model="password"/></p>
+    <p v-if="errMsg">{{ errMsg}}</p>
+    <p><button @click="register">Sign-In</button></p>
+    <p><button @click="signInWithGoogle">Sign In with Google</button></p>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+    import { useRouter } from 'vue-router'; //imports router
+    const email = ref("");
+    const password = ref("");
+    const errMsg = ref(""); // Error message
+    const router = useRouter()
+
+    const register = () => {
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((data) => {
+            console.log("Successfully signed in");
+            router.push('/todo');
+        })
+    const signInWithGoogle = () => {
+
+    }
+</script>
