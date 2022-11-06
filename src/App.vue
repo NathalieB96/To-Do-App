1857400faf0d9640b6fd05494ae37b130ab4d4af
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-06 16:16:31 +0100

    SignOut + check access permission

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     <router-link to="/todo">To-do</router-link>
     <router-link to="/signin">Login</router-link>
     <router-link to="/register">Register</router-link>
+    <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
   </nav>
 
   <div class="todo">
@@ -86,13 +87,45 @@
 
 <script setup>
 
+
+
 /*
   imports
 */
 
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'; //login + todo
+import { getAuth, onAuthStateChanged, signOut} from "firebase/auth";  //login
 import { collection, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from '@/firebase';
+
+
+/*
+  logout
+*/
+
+
+const isLoggedIn = ref(false);
+
+let auth;
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true;
+    }else {
+      isLoggedIn.value = false;
+    }
+  });
+});
+
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push("/signin")
+  })
+};
+
+
 
 /*
   firebase refs
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 
 import { createRouter, createWebHistory} from "vue-router";
+import { getAuth, onAuthStateChanged} from "firebase/auth"; 
 
 
 const router = createRouter({
@@ -8,8 +9,36 @@ const router = createRouter({
         { path: "/", component: () => import("../pages/home.vue") },
         { path: "/register", component: () => import("../pages/register.vue") },
         { path: "/signin", component: () => import("../pages/signin.vue") },
-        { path: "/todo", component: () => import("../pages/todo.vue") },
+        { 
+            path: "/todo", 
+            component: () => import("../pages/todo.vue"),
+            meta: {
+                requiresAuth: true
+            }, 
+        },
     ]
   })
+
+  const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged (
+            getAuth(),
+            reject
+        );
+    });
+  };
+
+  router.beforeEach((to, from, next ) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (getAuth().currentUser) {
+            next();
+        } else {
+            alert("you don't have access!")
+            next("/signin")
+        }
+    } else {
+        next();
+    }
+  });
 
   export default router;
