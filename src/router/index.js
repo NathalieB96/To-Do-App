9c2113a06fe15c81938a6cb41045c23a835a9878
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-06 16:24:23 +0100

    Fix: stay logged in after refresh
    - with onAuthStateChanged method from firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 
 import { createRouter, createWebHistory} from "vue-router";
 import { getAuth, onAuthStateChanged} from "firebase/auth"; 
+import { async } from "@firebase/util";
 
 
 const router = createRouter({
@@ -23,14 +24,18 @@ const router = createRouter({
     return new Promise((resolve, reject) => {
         const removeListener = onAuthStateChanged (
             getAuth(),
+            (user) => {
+                removeListener();
+                resolve(user);
+            },
             reject
         );
     });
   };
 
-  router.beforeEach((to, from, next ) => {
+  router.beforeEach(async(to, from, next ) => {
     if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if (getAuth().currentUser) {
+        if (await getCurrentUser()) {
             next();
         } else {
             alert("you don't have access!")
