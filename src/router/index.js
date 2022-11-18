6ef2b44ea6669ca56121fdf9492fe9d938876505
Nathalie <[nathalie.buettner@code.berlin]>
2022-11-18 12:14:04 +0100

    Redirect to signup page

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,9 @@ import { async } from "@firebase/util";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: () => import("../pages/home.vue") },
         { path: "/register", component: () => import("../pages/register.vue") },
         { path: "/signin", component: () => import("../pages/signin.vue") },
+        { path: "/", redirect: "/signin" },
         { 
             path: "/todo", 
             component: () => import("../pages/todo.vue"),
